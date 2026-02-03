b5c35a37ab0dafb8f83607d0ace892db86292498
Tiago Silva <eng.tiago.silva@gmail.com>
2026-02-03 17:08:19 +0000

    Update version number to 2.1.3 in __init__.py for PDF compressor

--- src/pdf_compressor/__init__.py
+++ src/pdf_compressor/__init__.py
@@ -1,6 +1,6 @@
 """PDF Compressor - Reliable PDF compression using multiple strategies."""
 
-__version__ = "2.1.2"
+__version__ = "2.1.3"
 __author__ = "Tiago Silva"
 
 from pdf_compressor.core.compressor import PDFCompressor, CompressionOutcome
